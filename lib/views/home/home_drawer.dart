686f5a0ed25f11a718b4edf7e99520fe201b4406
ritamk <ritamkonar7@gmail.com>
2022-04-07 01:22:03 +0530

    added option to access completed profile

--- lib/views/home/home_drawer.dart
+++ lib/views/home/home_drawer.dart
@@ -12,9 +12,12 @@ class HomeDrawer extends StatefulWidget {
 }
 
 class _HomeDrawerState extends State<HomeDrawer> {
+  late bool profileCompleted;
+
   @override
   void initState() {
     super.initState();
+    profileCompleted = widget.userProfileData.name!.isNotEmpty ? true : false;
   }
 
   @override
@@ -26,25 +29,19 @@ class _HomeDrawerState extends State<HomeDrawer> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                // const CircleAvatar(
-                //   maxRadius: 50.0,
-                //   child: Icon(
-                //     Icons.person_outline_rounded,
-                //     size: 80.0,
-                //     color: Colors.white,
-                //   ),
-                //   backgroundColor: Colors.grey,
-                // ),
                 const Text("Welcome\n",
                     style: TextStyle(fontSize: 20.0, color: Colors.black54)),
-                widget.userProfileData.name!.isNotEmpty
+                profileCompleted
                     ? Text(widget.userProfileData.name!)
                     : TextButton(
                         onPressed: () {
                           Navigator.of(context).pop();
-                          Navigator.of(context).push(CupertinoPageRoute(
+                          Navigator.of(context).push(
+                            CupertinoPageRoute(
                               builder: (context) =>
-                                  const ProfilePage(profileCompleted: false)));
+                                  const ProfilePage(profileCompleted: false),
+                            ),
+                          );
                         },
                         child: const Text(
                           "Complete your profile",
@@ -54,12 +51,33 @@ class _HomeDrawerState extends State<HomeDrawer> {
               ],
             ),
           ),
-          // const ListTile(
-
-          // ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text("Profile"),
+            onTap: () {
+              Navigator.of(context).push(
+                CupertinoPageRoute(
+                  builder: (context) => profileCompleted
+                      ? const ProfilePage(profileCompleted: true)
+                      : const ProfilePage(profileCompleted: false),
+                ),
+              );
+            },
+          ),
         ],
       ),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
     );
   }
 }
+
+
+// const CircleAvatar(
+  //   maxRadius: 50.0,
+  //   child: Icon(
+  //     Icons.person_outline_rounded,
+  //     size: 80.0,
+  //     color: Colors.white,
+  //   ),
+  //   backgroundColor: Colors.grey,
+// ),
--- lib/views/profile/profile.dart
+++ lib/views/profile/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jhatpat/models/user.dart';
 import 'package:jhatpat/services/database/database.dart';
 import 'package:jhatpat/services/providers.dart';
 import 'package:jhatpat/services/shared_pref.dart';
@@ -9,9 +10,11 @@ import 'package:jhatpat/shared/loading.dart';
 import 'package:jhatpat/shared/snackbars.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key, required this.profileCompleted})
+  const ProfilePage(
+      {Key? key, required this.profileCompleted, this.userProfileData})
       : super(key: key);
   final bool profileCompleted;
+  final UserProfileData? userProfileData;
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -22,8 +25,18 @@ class _ProfilePageState extends State<ProfilePage> {
   final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
   final FocusNode _nameFocus = FocusNode();
   String _name = "";
+  late TextEditingController _nameController;
   final FocusNode _emailFocus = FocusNode();
   String _email = "";
+  late TextEditingController _emailController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: widget.userProfileData!.name);
+    _emailController =
+        TextEditingController(text: widget.userProfileData!.email);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +53,7 @@ class _ProfilePageState extends State<ProfilePage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 TextFormField(
+                  controller: _nameController,
                   decoration:
                       authTextInputDecoration("Name", Icons.person, null),
                   style: const TextStyle(color: Colors.black, fontSize: 16.0),
@@ -55,6 +69,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
                 const SizedBox(height: 10.0, width: 0.0),
                 TextFormField(
+                  controller: _emailController,
                   decoration:
                       authTextInputDecoration("Email", Icons.mail, null),
                   style: const TextStyle(color: Colors.black, fontSize: 16.0),
@@ -66,25 +81,27 @@ class _ProfilePageState extends State<ProfilePage> {
                   onFieldSubmitted: (val) => FocusScope.of(context).unfocus(),
                 ),
                 const SizedBox(height: 20.0, width: 0.0),
-                Consumer(builder: (context, ref, __) {
-                  return MaterialButton(
-                    onPressed: () => updateButton(context, ref),
-                    child: !loading
-                        ? const Text(
-                            "Update",
-                            style: TextStyle(fontSize: 16.0),
-                          )
-                        : const Loading(white: true),
-                    minWidth: double.infinity,
-                    elevation: 0.0,
-                    focusElevation: 0.0,
-                    highlightElevation: 0.0,
-                    color: Colors.black,
-                    textColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0)),
-                  );
-                })
+                Consumer(
+                  builder: (context, ref, __) {
+                    return MaterialButton(
+                      onPressed: () => updateButton(context, ref),
+                      child: !loading
+                          ? const Text(
+                              "Update",
+                              style: TextStyle(fontSize: 16.0),
+                            )
+                          : const Loading(white: true),
+                      minWidth: double.infinity,
+                      elevation: 0.0,
+                      focusElevation: 0.0,
+                      highlightElevation: 0.0,
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                    );
+                  },
+                )
               ],
             ),
           ),
